ca6ae93bbb44e1942671c9c699c6f2826d422947
NABIN777 <nirajan.khanal777@gmail.com>
2023-12-31 07:34:48 +0545

    modified

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstone.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_shopping.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
